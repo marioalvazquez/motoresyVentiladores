1c6c5a2e983f4a5a1e1a36c3a5606a9c20bb8324
Mario Vázquez <marioal.vazquez@gmail.com>
2016-01-25 18:38:28 -0600

    Se agregó primer corrección de items individuales responsive exitosa(age.php)

--- marcas/Solerandpalau/age.php
+++ marcas/Solerandpalau/age.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="../../css/animate.css">
 </head>
 <body>
-	<?php 
+	<?php
 		require('header.php');
 		require('section-top.php');
 	 ?>
@@ -19,19 +19,19 @@
 <section>
 	<h1>ACCESORIOS</h1>
 	<div class="contenedor">
-		<div class="fila centrado">
-			<div class="col-6">
+		<div class="fila centrado columnas">
+			<div class="col-6 centroResponsive">
 				<img src="img/AGE.png" class="imgdeProducto">
 			</div>
-			<div class="col-4 alineacionDefault">
+			<div class="col-4 alineacionDefault centroResponsive">
 				<div class="contenedor">
 				<div class="fila">
 				<h3 class="tituloProductos col-12">AGE</h3>
 				<h4 class="col-12">Axiales con Transmisión</h4>
 				<p class="col-12">
-					Extractores Helicoidales con Persiana 800, 1000 y 1250 mm. de diámetro. Construcción en lámina galvanizada, malla de protección en aspiración, transmisión polea y banda, a 4 polos, caja de cojinetes S&P con rodamiento a bolas, hélices en 6 álabes con alineación y balanceo preciso, mecanismo centrífugo de apertura y cierre de persianas fabricado en resina termoplástica. 
+					Extractores Helicoidales con Persiana 800, 1000 y 1250 mm. de diámetro. Construcción en lámina galvanizada, malla de protección en aspiración, transmisión polea y banda, a 4 polos, caja de cojinetes S&P con rodamiento a bolas, hélices en 6 álabes con alineación y balanceo preciso, mecanismo centrífugo de apertura y cierre de persianas fabricado en resina termoplástica.
 
-Aplicaciones: 
+Aplicaciones:
 Bodegas comerciales e industriales, ventilación ambiental en industria, grandes almacenes comerciales, gimnasios, etc.
 
 
