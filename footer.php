b176cc5afd2c256769895bdfc0f1e45eb6839632
Mario Vázquez <marioal.vazquez@gmail.com>
2016-01-26 18:07:32 -0600

    Cambio de footer para función fixed bottom

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
-<div class="contenedorInfo">
-	<div class="contenedor">
-		<div class="fila centrado">
-			<div class="col-3 bordeHover">
-				<h3>Llámanos</h3>
+<footer class="footer">
+	<div class="footer-lista">
+			<div class="footer-lista-li">
+				<h3 class="footer-lista-li-titulo">
+					Llámanos
+				</h3>
 			</div>
-			<div class="col-3 bordeHover">
-				<h3>Horario</h3>
+			<div class="footer-lista-li horario">
+				<h3 class="footer-lista-li-titulo">
+					Horario
+				</h3>
 			</div>
-			<div class="col-3 bordeHover borderHoverlast">
-				<h3>Garantía Extendida</h3>
+			<div class="footer-lista-li">
+				<h3 class="footer-lista-li-titulo">
+					Garantía Extendida
+				</h3>
 			</div>
-		</div>
-	</div>
 
-</div>
-<footer>
-		<div class="contenedor">
-			<div class="fila centrado">
-				<div><img src="http://motoresyventiladores.mx/img/logotipo%20contraste.png" class="imgMarca"></div>
-			</div>
-		</div>
-	</footer>
+	</div>
+	<div class="footer-logo">
+		<img src="http://motoresyventiladores.mx/img/logotipo%20contraste.png" alt="" class="footer-logo-img">
+	</div>
+</footer>
