4132c5cada6f199fdeb59bf34bec4283b7752cc1
Mario Vázquez <marioal.vazquez@gmail.com>
2016-02-25 00:19:13 -0600

    Primer testing de errors

--- marcas/Solerandpalau/contact.php
+++ marcas/Solerandpalau/contact.php
@@ -4,6 +4,21 @@
   require_once 'libs/phpmailer/PHPMailerAutoload.php';
   $errors = [];
   if(isset($_POST['nombre'], $_POST['correo'], $_POST['telefono'], $_POST['mensaje'])){
-    echo 'All set';
+    $fields = [
+      'nombre' => $_POST['nombre'],
+      'correo' => $_POST['correo'],
+      'telefono' => $_POST['telefono'],
+      'mensaje' => $_POST['mensaje']
+    ];
+    foreach($fields as $field => $data) {
+      if(empty($data)){
+        $errors[] = 'The ' . $field . ' field is required.';
+      }
+    }
+  } else{
+    $errors[] = 'Something went wrong.';
   }
+
+  $_SESSION['errors'] = $errors;
+  header('Location: correos.php');
  ?>
--- marcas/Solerandpalau/correos.php
+++ marcas/Solerandpalau/correos.php
@@ -1,3 +1,9 @@
+<?php
+  session_start();
+
+  print_r($SESSION['errors']);
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
