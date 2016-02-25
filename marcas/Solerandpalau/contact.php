443a46d10b3e95b2eef5c0a81d92d159836d7d94
Mario Vázquez <marioal.vazquez@gmail.com>
2016-02-24 23:35:50 -0600

    Segunda prueba de contactPhpMailForm

---
+++ marcas/Solerandpalau/contact.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+
+  require_once 'libs/phpmailer/PHPMailerAutoload.php';
+  $errors = [];
+  print_r($_POST);
+  if(isset($_POST['name'])){
+
+  }
+ ?>
---
+++ marcas/Solerandpalau/success.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="../../css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="../../css/normalize.css">
+	<title>BS - Motores y Ventiladores</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0"/>
+	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../../css/animate.css">
+</head>
+<body>
+	<?php
+		require('header.php');
+		require('section-top.php');
+	 ?>
+
+<section>
+	<h1>ACCESORIOS</h1>
+	<div class="contenedor">
+	<h2>CORREO ENVIADO, MUCHAS GRACIAS!</h2>
+	</div>
+</section>
+
+<?php require('../../footer.php'); ?>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.js"></script>
+	<script type="text/javascript" src="formulario.js"></script>
+</body>
+</html>
