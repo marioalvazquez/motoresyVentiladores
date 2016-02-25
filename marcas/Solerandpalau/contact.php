19bbad74f8d900bf46613f7f9d96632fc8d1a8da
Mario Vázquez <marioal.vazquez@gmail.com>
2016-02-25 00:05:12 -0600

    All set for PHP Mail

--- marcas/Solerandpalau/contact.php
+++ marcas/Solerandpalau/contact.php
@@ -3,7 +3,6 @@
 
   require_once 'libs/phpmailer/PHPMailerAutoload.php';
   $errors = [];
-  print_r($_POST);
   if(isset($_POST['nombre'], $_POST['correo'], $_POST['telefono'], $_POST['mensaje'])){
     echo 'All set';
   }
